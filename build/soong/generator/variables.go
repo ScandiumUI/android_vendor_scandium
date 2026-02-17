b3855dcfdd4533870f076ea8a94f4e6e57ffd7a1
SayuZX <todraihan95@gmail.com>
2026-02-17 22:53:02 +0700

    build: rebrand soong modules and Go source
    - soong-scandium-generator, scandiumVarsPlugin
    - scandiumExpandVariables, scandiumVars
    - Package paths updated

--- build/soong/generator/variables.go
+++ build/soong/generator/variables.go
@@ -6,12 +6,12 @@ import (
 	"android/soong/android"
 )
 
-func lineageExpandVariables(ctx android.ModuleContext, in string) string {
-	lineageVars := ctx.Config().VendorConfig("lineageVarsPlugin")
+func scandiumExpandVariables(ctx android.ModuleContext, in string) string {
+	scandiumVars := ctx.Config().VendorConfig("scandiumVarsPlugin")
 
 	out, err := android.Expand(in, func(name string) (string, error) {
-		if lineageVars.IsSet(name) {
-			return lineageVars.String(name), nil
+		if scandiumVars.IsSet(name) {
+			return scandiumVars.String(name), nil
 		}
 		// This variable is not for us, restore what the original
 		// variable string will have looked like for an Expand
