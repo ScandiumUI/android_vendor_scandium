9b3df30dc1401b605e7b31cf25e7b3a70062496a
SayuZX <todraihan95@gmail.com>
2026-02-18 00:31:21 +0700

    scandiumd: rewrite entry point with signal handling

--- scandiumd/src/main.cpp
+++ scandiumd/src/main.cpp
@@ -2,13 +2,37 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "ScandiumPerfd.h"
+#include "GameDetector.h"
 
 #include <android-base/logging.h>
+#include <android-base/properties.h>
+
+#include <signal.h>
+
+using namespace scandium;
+
+static ScandiumPerfd* g_daemon = nullptr;
+
+static void signal_handler(int sig) {
+    if (g_daemon) g_daemon->request_shutdown();
+}
 
 int main() {
     android::base::InitLogging(nullptr, &android::base::KernelLogger);
-    LOG(INFO) << "scandiumd: ScandiumUI Performance Daemon starting";
 
-    scandium::ScandiumPerfd daemon;
-    return daemon.run();
+    struct sigaction sa;
+    sa.sa_handler = signal_handler;
+    sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGTERM, &sa, nullptr);
+    sigaction(SIGINT, &sa, nullptr);
+
+    ScandiumPerfd daemon;
+    g_daemon = &daemon;
+
+    int ret = daemon.run();
+
+    g_daemon = nullptr;
+    LOG(INFO) << "scandiumd: shutdown complete, exit=" << ret;
+    return ret;
 }
